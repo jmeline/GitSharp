9c35e5efe35fab268ad8d5f86ae9a128ff46805d
Jacob Meline <6642964+jmeline@users.noreply.github.com>
2025-04-06 00:02:05 -0600

    Add hasher

---
+++ GitSharp/Hasher.cs
@@ -0,0 +1,24 @@
+namespace GitSharp;
+
+public enum ObjectHeaderTypes
+{
+    Blob,
+    Commit,
+    Tag,
+    Tree
+}
+
+public static class Hasher
+{
+    public static string Serialize(ObjectHeaderTypes objectHeader, object data)
+    {
+        
+        return string.Empty;
+    }
+
+    public static string Deserialize()
+    {
+        return string.Empty;
+
+    }
+}
